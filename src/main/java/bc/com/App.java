4936227e08148723bbc638841e70e8c12d86af3b
Bacha Wondimu <bwondim3@montgomerycollege.edu>
2024-01-25 23:52:25 -0600

    first-commit

---
+++ src/main/java/bc/com/App.java
@@ -0,0 +1,14 @@
+package bc.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("this project has been creating from cli for the purpose of CI/CD demo");
+        System.out.println( "Hello World!" );
+    }
+}
